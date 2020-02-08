637508ce68bde4e8f386d49085113b26c68e45e4
徳岡　純次 <junji_tokuoka@wao-corp.com>
2020-02-08 15:01:26 +0900

    とりあえず7-6まで

--- lib/ui/dictionary.dart
+++ lib/ui/dictionary.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widget/drawer_menu.dart';
-
-//////////////////////////////////////////////
-//①importを追加
 import './input.dart';
-//////////////////////////////////////////////
+//①importを追加
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Dictionary extends StatefulWidget {
   @override
@@ -34,17 +32,12 @@ class _Dictionary extends State<Dictionary>
           IconButton(
             icon: Icon(Icons.add_box),
             onPressed: () {
-
-//////////////////////////////////////////////
-              //②追加ボタンを押した際にinputFORMが表示される
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (BuildContext context) => InputForm(),
                 ),
               );
-//////////////////////////////////////////////
-
             },
           ),
         ],
@@ -59,11 +52,117 @@ class _Dictionary extends State<Dictionary>
           controller: tabController,
           children: tabs.map(
                 (Tab tab) {
-              return Text(tab.text);
+              //②tabによって表示させる内容を変更する
+              return SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[buildStreamBuilder(tab.text)],
+                ),
+              );
             },
           ).toList(),
         ),
       ),
     );
   }
+
+  //③tabの値によってfirestoreから取ってくる値を変更
+  //Firebassの動作はflutterの本筋から外れるためハンズオンでは説明を省きます
+  //詳細はコメントを参照してください
+  Widget buildStreamBuilder(String tab) {
+    //tabのテキストによってqueryタイプを変更
+    String queryType = "";
+    if (tab == '英和') {
+      queryType = 'en';
+    } else {
+      queryType = 'ja';
+    }
+
+    //非同期処理
+    //値変更次第viewの状態が変更される
+    return StreamBuilder<QuerySnapshot>(
+      //firestoreのコレクションから値を取得
+      stream: Firestore.instance
+          .collection("dictionary")
+          .where('type', isEqualTo: queryType)
+          .orderBy('created_at', descending: true)
+          .snapshots(),
+      //値取得時の動作
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        //エラー時の処理
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        //コネクションの状態によって表示変更
+        switch (snapshot.connectionState) {
+        //waiting時
+          case ConnectionState.waiting:
+            return Text("Loading...");
+        //正常接続時
+          default:
+          //④表示デザインを整える
+            return Align(
+              alignment: Alignment.topCenter,
+              //複数Widgetが入るためColumn利用
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                //⑤firestoreから取得した値を元にウィジェット作成
+                //List<DocumentSnapshot> documentsからDocumentSnapshotを一つずつ取り出して処理
+                children: snapshot.data.documents.map(
+                      (DocumentSnapshot document) {
+                    return Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          //可変に変更されるウィジェットなのでメソッド化する
+
+//////////////////////////////////////////////
+                          //⑥コメントを外す
+                          createWordTile(document),
+//////////////////////////////////////////////
+
+                          //createButtonBar(document),
+                        ],
+                      ),
+                    );
+                  },
+                ).toList(),
+              ),
+            );
+        }
+      },
+    );
+  }
+
+//////////////////////////////////////////////
+//⑥個別のワードを表示
+  Widget createWordTile(DocumentSnapshot document) {
+    //connectionが正常でも翻訳が間に合わないケースがあるため
+    if (document['translated'] == null) {
+      return Text("Loading...");
+    }
+
+    //英和か和英のtypeによって表示させるか制御する
+    if (document['type'] == 'en') {
+      return ListTile(
+        leading: const Icon(Icons.book),
+        title: Text(document['word']),
+        subtitle: Text(
+          "\n意味 ： " + document['translated']['ja'].toString(),
+        ),
+      );
+    } else if (document['type'] == 'ja') {
+      return ListTile(
+        leading: const Icon(Icons.book),
+        title: Text(document['word']),
+        subtitle: Text(
+          "\n意味 ： " + document['translated']['en'].toString(),
+        ),
+      );
+    } else {
+      return Text("Error");
+    }
+  }
+//////////////////////////////////////////////
 }
--- lib/ui/input.dart
+++ lib/ui/input.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+//①firestoreインポート
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-//①Form用の構造体を用意
 class FormData {
-  //typeの初期値
   String type = "en";
   String word = "";
 }
@@ -15,6 +15,11 @@ class InputForm extends StatefulWidget {
 }
 
 class MyInputFormState extends State<InputForm> {
+  //②firestore用にリファレンス用意
+  DocumentReference mainReference =
+  Firestore.instance.collection('dictionary').document();
+  //③form用のkeyを用意
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   FormData data = FormData();
 
   @override
@@ -22,65 +27,72 @@ class MyInputFormState extends State<InputForm> {
     return Scaffold(
       appBar: AppBar(
         title: Text('ワード登録'),
-        //②保存用のボタンを設定
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.save),
             onPressed: () {
-              //本来保存処理が入るはずだが今はまだ画面を閉じる機能だけ
-              Navigator.pop(context);
+
+//////////////////////////////////////////////
+              //④保存時処理
+              //validate＆save(テキストフィールドのsave処理が走る)＆firestore保存処理
+              if (formKey.currentState.validate()) {
+                formKey.currentState.save();
+                mainReference.setData(
+                  {
+                    'type': data.type,
+                    'word': data.word,
+                    'created_at': DateTime.now(),
+                  },
+                );
+                Navigator.pop(context);
+//////////////////////////////////////////////
+
+              }
             },
           ),
         ],
       ),
       body: SafeArea(
-        //③Formを設定
         child: Form(
-          //④Formは複数ウィジェットの組み合わせなのでListViewを利用
+
+//////////////////////////////////////////////
+          //⑤formにkey設定
+          key: formKey,
+//////////////////////////////////////////////
+
           child: ListView(
             padding: EdgeInsets.all(20.0),
             children: <Widget>[
-              //⑤英和用Radioボタン
               RadioListTile(
                 value: "en",
-                //data構造体を利用して和英と状態を共有
                 groupValue: data.type,
-                //ラジオボタン横に表示されるテキスト
                 title: Text("英和"),
                 onChanged: (String value) {
-                  //変更時のイベント挙動
                   setState(
-                    () {
-                      //構造体のtypeの値をvalue(en)に設定
+                        () {
                       data.type = value;
                     },
                   );
                 },
               ),
-              //⑥和英用Radioボタン
               RadioListTile(
                 value: "ja",
                 groupValue: data.type,
                 title: Text("和英"),
                 onChanged: (String value) {
                   setState(
-                    () {
+                        () {
                       data.type = value;
                     },
                   );
                 },
               ),
-
-//////////////////////////////////////////////
-              //⑦ワード入力用テキストフィールド
               TextFormField(
-                //テキストフィールドのデコレーション
                 decoration: InputDecoration(
                   icon: Icon(Icons.library_books),
                   hintText: 'ワード',
                   labelText: 'word',
                 ),
-                //保存時のイベント挙動
                 onSaved: (String value) {
                   setState(
                     () {
@@ -88,17 +100,14 @@ class MyInputFormState extends State<InputForm> {
                     },
                   );
                 },
-                //バリデーション時の挙動
                 validator: (value) {
                   if (value.isEmpty) {
                     return 'ワードは必須入力項目です';
                   }
-                  //バリデーションクリアのときはreturn nullしないとワーニング
                   return null;
                 },
                 initialValue: data.word,
               ),
-//////////////////////////////////////////////
             ],
           ),
         ),
